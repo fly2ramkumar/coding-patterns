f65c83aa61ff2aca0e2ca2ea6b44a3e5171549ab
Ramkumar R <Ramkumar R@LAPTOP-IPV094T9>
2024-06-12 12:23:10 +0530

    Palindrome changes

--- src/main/java/two_pointers/ValidPalindrome.java
+++ src/main/java/two_pointers/ValidPalindrome.java
@@ -2,6 +2,24 @@ package two_pointers;
 
 public class ValidPalindrome {
 	
+	
+	// Time Complexity - O(n)
+	// Space complexity - O(n)
+	public static boolean isPalindromeNaiveApproach(String str){ 
+		
+		StringBuilder reverseStrBuilder = new StringBuilder();
+
+		for(int i=str.length()-1; i>=0; i--) {
+			char selectedChar = str.charAt(i);
+			reverseStrBuilder.append(selectedChar);
+		}
+		System.out.println("Reversed String  - "+reverseStrBuilder.toString());
+		return reverseStrBuilder.toString().equals(str);
+		
+	}
+	
+		// Time Complexity - O(n). Actual running time is n/2, since constants is removed from Big(O), /2 is removed. 
+		// Space complexity - O(1)
 	public static boolean isPalindrome(String str){ 
 		
 		int beginPointer = 0;
@@ -26,6 +44,9 @@ public class ValidPalindrome {
 	}
 	
 	public static void main(String[] args) {
+		
+		
+		
 		System.out.println("isValid Palindrome - "+isPalindrome("madam"));
 		System.out.println("isValid Palindrome - "+isPalindrome("madbm"));
 		
