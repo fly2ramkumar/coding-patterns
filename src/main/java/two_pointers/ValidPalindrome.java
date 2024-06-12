03118c3024c18474aef534cdec2ba8dda78381bc
Ramkumar R <Ramkumar R@LAPTOP-IPV094T9>
2024-06-12 11:54:39 +0530

    Palindrome changes

---
+++ src/main/java/two_pointers/ValidPalindrome.java
@@ -0,0 +1,37 @@
+package two_pointers;
+
+public class ValidPalindrome {
+	
+	public static boolean isPalindrome(String str){  //madam
+		
+		int beginPointer = 0;
+		int endPointer = str.length() - 1;	
+		
+		while(endPointer > beginPointer) {
+			
+			char startPos = str.charAt(beginPointer);
+			char endPos = str.charAt(endPointer);
+			//System.out.println("startPos - "+startPos+" endPos - "+endPos);
+			
+			if(startPos != endPos) {
+				return false;
+			}
+			
+			beginPointer = beginPointer + 1;
+			endPointer = endPointer - 1;
+			
+		}
+		
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("isValid Palindrome - "+isPalindrome("madam"));
+		System.out.println("isValid Palindrome - "+isPalindrome("madbm"));
+		
+		System.out.println("isValid Palindrome - "+isPalindrome("maddam"));
+		System.out.println("isValid Palindrome - "+isPalindrome("madbam"));
+	}
+	
+
+}
